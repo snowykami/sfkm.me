0197cc417b99ad6751ae81a8357f55927e752595
Snowykami <snowykami@outlook.com>
2025-02-04 01:09:33 +0800

    更新多语言支持，添加更多内容到语言数据

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -4,16 +4,20 @@ const langData: Record<string, Record<string, string>> = {
     "zh-CN": {
         "h1": "<h2>你好呀，这里是<span id='nickname'>Snowykami</span></h2>",
         "h2": "<h2>欢迎来到我的索引站</h2>",
+
         "p1": "后端开发者",
         "p2": "在轻雪工作室和红岩网校工作",
         "p3": "来自中国重庆",
+        "p4": "Minecraft，原神及更多..."
     },
     "en": {
         "h1": "<h2>Hi, I'm <span id='nickname'>Snowykami</span></h2>",
         "h2": "<h2>Welcome to my index site</h2>",
+
         "p1": "Backend Developer",
         "p2": "Work at Liteyuki and Redrock",
         "p3": "From Chongqing, China",
+        "p4": "Minecraft, Genshin Impact and more..."
     }
 }
 
