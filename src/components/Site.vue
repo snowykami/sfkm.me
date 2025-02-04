4dd27b052ab3c5e31827727465122615f173efe0
Snowykami <snowykami@outlook.com>
2025-02-04 18:03:12 +0800

    优化 Contact、Left、Readme、Right 和 Site 组件，更新样式和国际化文本，新增社交链接图标

--- src/components/Site.vue
+++ src/components/Site.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps} from 'vue';
+import { defineProps } from 'vue';
 
 defineProps({
     name: String,
@@ -11,26 +11,34 @@ defineProps({
 <template>
     <a target="_blank" class="site container ui react-ui size" :href="url">
         <div class="container" id="site">
-        <div class="site-name">{{ name }}</div>
-        <div class="site-url">{{ url }}</div>
-    </div>
+            <div class="site-name">{{ name }}</div>
+            <div class="site-url">{{ url }}</div>
+        </div>
     </a>
 </template>
 
 <style lang="css" scoped>
-.site{
-    border-bottom: none;
+a {
+    border-bottom: none !important;
+    text-decoration: none;
+}
+
+.site {
     padding-bottom: 0;
 }
 
-.site-name{
+.site-name {
     font-size: 1em;
     font-weight: bold;
     margin-bottom: 0.5em;
 }
 
-.site-url{
+.site-url {
     font-size: 0.8em;
-    color: #666;
+    color: #b0b0b0;
+}
+
+.site:hover {
+    background-color: var(--theme-color);
 }
 </style>
