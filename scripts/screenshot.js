a2b7e03dd58a617dda4826235db771f67344685a
Snowykami <snowykami@outlook.com>
2025-05-26 22:16:11 +0800

    feat: 在截图功能中设置视口大小以确保一致的截图效果

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -4,6 +4,7 @@ import path from 'path'
 async function main() {
   const browser = await chromium.launch()
   const page = await browser.newPage()
+  await page.setViewportSize({ width: 1920, height: 1080 })
   await page.goto('https://sfkm.me', { waitUntil: 'networkidle' })
   const screenshotPath = path.resolve('./public/preview.png')
   await page.screenshot({ path: screenshotPath, fullPage: true })
