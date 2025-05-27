afa68aba7f3e8162d0916640ba905b073edabb47
Snowykami <snowykami@outlook.com>
2025-05-28 01:32:11 +0800

    feat: 添加 viewport meta 标签以优化移动设备显示

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
     <html lang={getLang()}>
       <head>
         <link rel="icon" href="https://q.qlogo.cn/g?b=qq&nk=2751454815&s=640" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
