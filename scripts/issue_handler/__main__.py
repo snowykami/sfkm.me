8b62e57f11e9c4de32c9672a394948fccd961b3b
Snowykami <snowykami@outlook.com>
2025-05-30 15:32:29 +0800

    feat: Enhance issue handling with new models and methods
    - Added Repo, Event, and extended Comment models to better structure issue data.
    - Introduced ClientInterface methods for fetching and editing files in repositories.
    - Implemented GitHubClient and GiteaClient methods for issue and comment management.
    - Created IssueContext for managing issue-related actions and added methods for comment handling.
    - Added markdown_parser for parsing structured data from Markdown text.
    - Introduced constants for user roles in issue handling.
    - Updated dependencies in uv.lock to include aiofiles and beautifulsoup4.

--- scripts/issue_handler/__main__.py
+++ scripts/issue_handler/__main__.py
@@ -1,16 +1,22 @@
 import asyncio
 import os
-from models import ActionIssueContext, GitHubClient
+from models import IssueContext, GitHubClient
 from friend_link_handler import handle_friend_link_issue
 
 async def main():
-    ctx = ActionIssueContext(
-        issue_number=int(os.getenv("GITHUB_EVENT_ISSUE_NUMBER", "0")),
-        repository_name=os.getenv("GITHUB_REPOSITORY", ""),
-        event_action=os.getenv("GITHUB_EVENT_ACTION", ""),
-        client=GitHubClient(os.getenv("GITHUB_TOKEN", "")),
-        title=os.getenv("GITHUB_EVENT_ISSUE_TITLE", ""),
-        body=os.getenv("GITHUB_EVENT_ISSUE_BODY", "")
+    event_name = os.getenv("GITHUB_EVENT_NAME", "issue")
+    event_action = os.getenv("GITHUB_EVENT_ACTION", "opened")
+    issue_number = os.getenv("GITHUB_EVENT_ISSUE_NUMBER", "3")
+    issue_comment_id = os.getenv("GITHUB_EVENT_COMMENT_ID", "0")
+    repository_name=os.getenv("GITHUB_REPOSITORY", "snowykami/sfkm.me")
+    
+    ctx = await IssueContext.new(
+        client=GitHubClient(token=os.getenv("GITHUB_TOKEN", "")),
+        repository_name=repository_name,
+        event_name=event_name,
+        event_action=event_action,
+        issue_number=int(issue_number),
+        comment_id=int(issue_comment_id),
     )
     await handle_friend_link_issue(ctx)
 
