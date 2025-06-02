0a68b2ad6431bf213db9105b605f56891afb4c96
Snowykami <snowykami@outlook.com>
2025-06-03 06:01:23 +0800

    Update screenshot.js

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -4,7 +4,7 @@ import path from 'path'
 async function screenshotWithTheme(theme, filename) {
   const browser = await chromium.launch()
   const page = await browser.newPage()
-  await page.setViewportSize({ width: 2560, height: 1600 })
+  await page.setViewportSize({ width: 2560, height: 1440 })
   await page.emulateMedia({ colorScheme: theme }) // 指定主题
   await page.goto('https://sfkm.me/#music', { waitUntil: 'networkidle' })
   const screenshotPath = path.resolve(filename)
