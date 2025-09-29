33d5135e961b0566e55dff2d065c00861946090f
Snowykami <snowykami@outlook.com>
2025-09-29 18:33:07 +0800

    feat: 优化课程项样式，调整边框和背景颜色

--- src/apps/Contacts.tsx
+++ src/apps/Contacts.tsx
@@ -131,7 +131,7 @@ function CourseItem({ course }: { course: SimplifyCourse }) {
       className={`flex rounded-xl items-center justify-between mt-3 py-2 border-1
         ${isCurrent ? "border-emerald-300" :
           isPassed ? "bg-slate-300/80 dark:bg-slate-800/50 border-slate-300 dark:border-slate-600" :
-            " bg-slate-100/80 dark:bg-slate-800/50 border-slate-200 dark:border-slate-600"} 
+            " bg-slate-100/80 dark:bg-slate-600/50 border-slate-100 dark:border-slate-500"} 
       `}
       style={isCurrent ? { background: `linear-gradient(90deg, ${progressColorPassed} ${progressPercent}%, ${progressColorRemaining} ${progressPercent}%)` } : {
       }}
